fd5b86b586ac9be47cc36ec752065554bd0a48ef
conloppri <consueloloppri@gmail.com>
2026-05-14 23:27:28 +0200

    Mejora de formularios en parcelaView y personalView, mejors en estilos_prueba.css

--- views/parcelasView.php
+++ views/parcelasView.php
@@ -12,44 +12,43 @@ $parcelas = obtenerInfoParcelas($connection, $explotacion_id);
 <div class="main_content">
     <div class="cabecera_parcelas">
         <h1>Parcelas y unidades de gestión</h1>
-        <button type="button" class="add_button" onclick="abrirFormulario()">+ Añadir parcela</button>
+        <button type="button" class="add_button" onclick="abrirFormularioParcela()">+ Añadir parcela</button>
     </div>
 
     <!-- Formulario para añadir nueva parcela -->
-    <div class="modal_formulario" id="modalFormulario">
-        <h3>Nueva parcela</h3>
-        <form action="guardar.php" method="POST">
-            <label for="nombre">Nombre:</label>
-            <input type="text" name="nombre" placeholder="Nombre">
-            <br><br>
-            <div style="align-items: center;">
-                <label for="provincia">Provincia:</label>
-                <select name="provincia" id="provincia">
-                    <option value="" selected disabled>Selecciona provincia</option>
-                    <?php
-                    $provincias = obtenerProvincias($connection);
-                    foreach ($provincias as $provincia) {
-                        echo "<option value='" . $provincia['provincia_id'] . "'>" . htmlspecialchars($provincia['nombre']) . "</option>";
-                    }
-                    ?>
-                </select>
-                <label for="municipio">Municipio:</label>
-                <select name="municipio" id="municipio">
-                    <option value="" selected disabled>Selecciona municipio</option>
-                </select>
-            </div>
-            <br><br>
-            <div style="align-items: center;">
-                <label for="superficie">Superficie (ha):</label>
-                <input type="number" step="0.1" name="superficie" placeholder="Superficie">
-                <label for="sigpac">SIGPAC:</label>
-                <input type="text" name="sigpac" placeholder="SIGPAC">
-                <br><br>
-            </div>
-            <button type="submit">Guardar</button>
-            <button type="button" onclick="cerrarFormulario()">Cancelar</button>
-        </form>
-    </div>
+    <dialog id="parcela_modal" class= "modal_formulario">
+        <div>
+            <h2 style="text-align: center;">Nueva parcela</h2>
+            <form action="guardar.php" method="POST">
+                <div class="grupo-form">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" name="nombre" placeholder="Nombre">
+                    <label for="provincia">Provincia:</label>
+                    <select name="provincia" id="provincia">
+                        <option value="" selected disabled>Selecciona provincia</option>
+                        <?php
+                        $provincias = obtenerProvincias($connection);
+                        foreach ($provincias as $provincia) {
+                            echo "<option value='" . $provincia['provincia_id'] . "'>" . htmlspecialchars($provincia['nombre']) . "</option>";
+                        }
+                        ?>
+                    </select>
+                    <label for="municipio">Municipio:</label>
+                    <select name="municipio" id="municipio">
+                        <option value="" selected disabled>Selecciona municipio</option>
+                    </select>
+                    <label for="superficie">Superficie (ha):</label>
+                    <input type="number" step="0.1" name="superficie" placeholder="Superficie">
+                    <label for="sigpac">SIGPAC:</label>
+                    <input type="text" name="sigpac" placeholder="SIGPAC">
+                </div>
+                <div style="display: flex; justify-content: space-around; margin-top: 20px;">
+                    <button class="add_button" type="submit">Guardar</button>
+                    <button  class="cancel_button" type="button" onclick="cerrarFormularioParcela()">Cancelar</button>
+                </div>
+            </form>
+        </div>
+    </dialog>
 
 
     <div class="table_exp">
@@ -99,28 +98,29 @@ $parcelas = obtenerInfoParcelas($connection, $explotacion_id);
                                 </tbody>
                             </table>
                             <button type="button" class="add_button" onclick="abrirFormularioUniGest()">+ Añadir unidad de gestión</button>
-                            <div class="modal_formulario" id="modalUniGes">
-                                <h3>Nueva unidad de gestión</h3>
-                                <form action="guardar.php" method="POST">
-                                    <label for="nombre">Nombre:</label>
-                                    <input type="text" name="nombre" placeholder="Nombre">
-                                    <br><br>
-                                    <label for="superficie">Superficie (ha):</label>
-                                    <input type="number" step="0.1" name="superficie" placeholder="Superficie">
-                                    <br><br>
-                                    <label for="uso">Uso:</label>
-                                    <select name="uso" id="uso">
-                                        <option value="" selected disabled>Selecciona uso</option>
-                                        <option value="agrícola">Agrícola</option>
-                                        <option value="ganadero">Ganadero</option>
-                                        <option value="forestal">Forestal</option>
-                                        <option value="en descanso">En descanso</option>
-                                        <option value="otro">Otro</option>
-                                    </select>
-                                    <br><br>
-                                    <button type="submit">Guardar</button>
-                                    <button type="button" onclick="cerrarFormularioUniGest()">Cancelar</button>
-                                </form>
+                            <dialog class="modal_formulario" id="uniGestion_modal">
+                                <div>
+                                    <h2 style="text-align: center;">Nueva unidad de gestión</h2>
+                                    <div class="grupo-form">
+                                        <label for="nombre">Nombre:</label>
+                                        <input type="text" name="nombre" placeholder="Nombre">
+                                        <label for="superficie">Superficie (ha):</label>
+                                        <input type="number" step="0.1" name="superficie" placeholder="Superficie">
+                                        <label for="uso">Uso:</label>
+                                        <select name="uso" id="uso">
+                                            <option value="" selected disabled>Selecciona uso</option>
+                                            <option value="agrícola">Agrícola</option>
+                                            <option value="ganadero">Ganadero</option>
+                                            <option value="forestal">Forestal</option>
+                                            <option value="en descanso">En descanso</option>
+                                            <option value="otro">Otro</option>
+                                        </select>
+                                    </div>
+                                        <button type="submit" class="add_button">Guardar</button>
+                                        <button type="button" class="cancel_button" onclick="cerrarFormularioUniGest()">Cancelar</button>
+                                    </form>
+                                </div>
+                            </dialog>
                         </td>
                     <?php endforeach; ?>
             </tbody>
@@ -137,21 +137,20 @@ $parcelas = obtenerInfoParcelas($connection, $explotacion_id);
             fila.style.display = "none";
         }
     }
-
-    function abrirFormulario() {
-        document.getElementById("modalFormulario").style.display = "flex";
+    function abrirFormularioParcela() {
+        document.getElementById('parcela_modal').showModal();
     }
 
-    function cerrarFormulario() {
-        document.getElementById("modalFormulario").style.display = "none";
+    function cerrarFormularioParcela() {
+        document.getElementById('parcela_modal').close();
     }
 
     function abrirFormularioUniGest() {
-        document.getElementById("modalUniGes").style.display = "flex";
+        document.getElementById("uniGestion_modal").showModal();
     }
 
     function cerrarFormularioUniGest() {
-        document.getElementById("modalUniGes").style.display = "none";
+        document.getElementById("uniGestion_modal").close();
     }
 
     //Municpios por provincia
