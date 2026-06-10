d15412532baa1da668d1d071a570e1a5c1479a1c
conloppri <consueloloppri@gmail.com>
2026-06-10 23:20:51 +0200

    implementación de modficacion y eliminacion de parcelas y unidades de gestion

--- models/parcelaModel.php
+++ models/parcelaModel.php
@@ -12,6 +12,47 @@ function obtenerParcelas(int $idExplotacion) {
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function obtenerParcelaPorId(int $parcela_id){
+    global $connection;
+    $sql ="SELECT * FROM parcela WHERE parcela_id= :parcela_id";
+    $stmt = $connection->prepare($sql);
+    $stmt->execute(['parcela_id' => $parcela_id]);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function modificarParcela(int $parcela_id, string $nombreParcela, float $superficieParcela, array $subdivisionesSigpac){
+    global $connection;
+    $sql="UPDATE parcela 
+    SET nombre = :nombreParcela, 
+    superficie=:superficieParcela, 
+    provincia_id = :provincia, 
+    municipio_id= :municipio,
+    agregado = :agregado,
+    zona = :zona,
+    poligono = :poligono,
+    parcela = :parcela
+    WHERE parcela_id = :parcela_id";
+    $stmt = $connection->prepare($sql);
+    return $stmt->execute([
+        'nombreParcela' =>$nombreParcela,
+        'superficieParcela' =>$superficieParcela,
+        'provincia' => $subdivisionesSigpac['provincia_id'],
+        'municipio' => $subdivisionesSigpac['municipio_id'],
+        'agregado' => $subdivisionesSigpac['agregado'],
+        'zona' => $subdivisionesSigpac['zona'],
+        'poligono' => $subdivisionesSigpac['poligono'],
+        'parcela' => $subdivisionesSigpac['parcela'],
+        'parcela_id' => $parcela_id
+    ]);
+}
+
+function eliminarParcela(int $parcela_id){
+    global $connection;
+    $sql = "DELETE FROM parcela WHERE parcela_id =:idParcela";
+    $stmt = $connection->prepare($sql);
+    return $stmt->execute(['idParcela' => $parcela_id]);
+}
+
 // Función para obtener la información de las unidades de gestión de una parcela específica
 function obtenerUnidadesGestion(int $idParcela) {
     global $connection;
@@ -49,5 +90,29 @@ function guardarUnidadGestion(int $idParcela, string $nombreUniGestion, float $s
         'superficieUniGestion' => $superficieUniGestion,
         'uso' => $uso
     ]);
+}
+
+function modificarUnidadGestion(int $uniGes_id, string $nombre, float $superficie, string $uso){
+    global $connection;
+    $sql="UPDATE unidad_gestion 
+    SET nombre = :nombreUnidad, 
+    superficie=:superficieUnidad, 
+    uso= :usoUnidad
+    WHERE unidad_gestion_id = :unidad_id";
+
+    $stmt = $connection->prepare($sql);
+    return $stmt->execute([
+        'nombreUnidad' => $nombre,
+        'superficieUnidad' => $superficie,
+        'usoUnidad' => $uso,
+        'unidad_id' => $uniGes_id
+    ]);
+}
+
+function eliminarUnidad(int $unidad_id){
+    global $connection;
+    $sql = "DELETE FROM unidad_gestion WHERE unidad_gestion_id =:unidad_id";
+    $stmt = $connection->prepare($sql);
+    return $stmt->execute(['unidad_id' => $unidad_id]);
 }
 ?>
